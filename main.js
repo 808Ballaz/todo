730d35fd61713073bb1994bccee34ce5dca32fc7
sun choi <sun.choi@hired.com>
2016-08-04 16:40:29 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+$(function () {
+  $('.add-todo').on('click', function () {
+    var newTodo = $('.todo').first().clone();
+    newTodo.find('.todo-text').val('');
+    newTodo.appendTo('.todo-list');
+  });
+});
