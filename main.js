8ee887fee9672cf41c9915aa8398be4543ac9156
sun choi <sun.choi@hired.com>
2016-08-11 16:34:57 -0700

    To Do List Revisions

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
+function removeTodo (event) {
+  var target = event.currentTarget;
+  $(target).parent().remove();
+}
 
 $(function () {
   $('.add-todo').on('click', function () {
     var newTodo = $('.todo').first().clone();
-    newTodo.find('.todo-text').val('');
+    newTodo.removeClass('hide');
+    newTodo.find('.todo-remove').on('click', removeTodo);
     newTodo.appendTo('.todo-list');
   });
+
+  $('.todo-remove').on('click',removeTodo) ;
 });
